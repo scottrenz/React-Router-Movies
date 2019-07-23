063c8423aaf7fe634a4c2bb68d351f400b4a296f
Scott Renz <scottrenz@scottrenz.com>
2019-07-23 16:34:05 -0400

    Got it to get actor names out of an array and no longer mashed together

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom'
 
 const Movie = (props) => {
   const [movie, setMovie] = useState({});
+  const [star, setStar] = useState({stars: []});
   useEffect(() => {
     const id = 1;
     // change ^^^ that line and grab the id from the URL
@@ -12,6 +14,7 @@ const Movie = (props) => {
        .get(`http://localhost:5000/api/movies/${props.match.params.id}`)
        .then(response => {
         setMovie(response.data);
+        setStar({stars: response.data.stars});
       })
         .catch(error => {
           console.error(error);
@@ -43,19 +46,15 @@ const Movie = (props) => {
           Metascore: <strong>{metascore}</strong>
         </div>
         <h3>Actors</h3>
-
-        <div className="movie-star">
-             {movie.stars}
-           </div>
-         {/* {star.stars.map(star => (
-           <div key={star} className="movie-star">
-             {star}
-           </div>
-         ))} */}
-      </div>
+          {star.stars.map(star => (
+            <div key={star} className="movie-star">
+              {star}
+            </div>
+          ))} 
       <div onClick={ ()=> props.addToSavedList(movie) }  className="save-button">Save</div>
-   
-    </div>
+
+      </div>
+      </div>
   );
 }
 
